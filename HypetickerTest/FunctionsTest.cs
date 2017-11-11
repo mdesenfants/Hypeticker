9ee553dbd628725aedfd36bbecc0e6abb728c5b9
Matthew DesEnfants <matt.desenfants@gmail.com>
2017-11-11 15:39:31 -0800

    new test stubs

---
+++ HypetickerTest/FunctionsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HypetickerTest
+{
+    [TestClass]
+    public class FunctionsTest
+    {
+        [TestMethod]
+        public void GetStoriesTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetStoryTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ParseTitlesTest()
+        {
+
+        }
+    }
+}
